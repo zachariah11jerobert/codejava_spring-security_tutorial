84674e402d865a7c75a681bae4f12f5c4e7607d1
zachariah11jerobert <zachariah11jerobert@gmail.com>
2022-11-10 23:24:03 +0530

    done code

---
+++ src/main/java/net/codejavajerome/productmanager/WebSecurityConfig.java
@@ -0,0 +1,44 @@
+package net.codejavajerome.productmanager;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected UserDetailsService userDetailsService() {
+        UserDetails user1= User
+                .withUsername("jerome")
+                .password("test123")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1);
+    }
+
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+
+
+    }
+}
